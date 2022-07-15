1d21c522bf1a8ff1efb9c30242d264d82f8dba80
dizhong <dizhong@aistetic.com>
2022-07-15 09:40:53 +0100

    Update Teamcity_Test.py

--- Teamcity_Test.py
+++ Teamcity_Test.py
@@ -11,12 +11,16 @@ if __name__ == '__main__':
     python_version = '3.8'
     docker_folder = '/install_info'
 
-    # simulate the lambda environment by a docker
     subprocess.check_call(
         f'docker run -v {os.getcwd()}:{docker_folder} public.ecr.aws/sam/build-python{python_version} /bin/sh -c '
-        f'"ls -lFa  {docker_folder}; '
-        f'pip install -r {docker_folder}/{requirements_file} -t {docker_folder}/{output_dir}/python; '
-        f'exit"')
+        f'"ls -lFa  {docker_folder}; exit"')
+
+    # # simulate the lambda environment by a docker
+    # subprocess.check_call(
+    #     f'docker run -v {os.getcwd()}:{docker_folder} public.ecr.aws/sam/build-python{python_version} /bin/sh -c '
+    #     f'"ls -lFa  {docker_folder}; '
+    #     f'pip install -r {docker_folder}/{requirements_file} -t {docker_folder}/{output_dir}/python; '
+    #     f'exit"')
 
     # if not os.environ.get('SKIP_PIP'):
     #     subprocess.check_call(f'python -m pip install -r {requirements_file} -t {output_dir}/python'.split())
