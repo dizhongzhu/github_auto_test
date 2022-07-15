342c95a3d99030e07ffe2e9c917ef3eee681c094
dizhong <dizhong@aistetic.com>
2022-07-15 14:15:26 +0100

    Update Teamcity_Test.py

--- Teamcity_Test.py
+++ Teamcity_Test.py
@@ -4,21 +4,12 @@ Date: 15/07/2022
 """
 import subprocess
 import os
-import docker
 
 if __name__ == '__main__':
     requirements_file = f'/requirements.txt'
     output_dir = '.build'
     python_version = '3.8'
     docker_folder = '/install_info'
-
-    # client = docker.from_env()
-    # a = client.containers.run(f'public.ecr.aws/sam/build-python{python_version}',
-    #                           f'ls -lFa /',
-    #                           f'pip install -r {docker_folder}/{requirements_file} -t {docker_folder}/{output_dir}/python',
-    #                           volumes=[f'{os.getcwd()}:{docker_folder}']
-    #                           )
-    # print(a)
 
     subprocess.check_call([
         f'docker run --rm -v {os.getcwd()}:{docker_folder} public.ecr.aws/sam/build-python{python_version} /bin/sh -c '
